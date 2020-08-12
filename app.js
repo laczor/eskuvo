0375ebf6ab459107b77f109de94d43e2ea92050b
Laczovics Istvan <i.laczovics@gmail.com>
2020-08-12 14:39:37 +0200

    modify to have dynamic port

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 
 app.use(express.static('dist'));                                                      //For external styling and functionality + js, we can enable our server with express to provide the requested files to anybody
 
-app.listen( 3000, function() {
-// app.listen(process.env.PORT, process.env.IP, function() {
+// app.listen( 3000, function() {
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log('CV Website went online on port 3000');
 });                                                                                     // Tells express to listen to a certain port, which c9.io is telling to listen, on IP which c9.io is telling
 
